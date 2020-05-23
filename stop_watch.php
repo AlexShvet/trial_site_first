c8a0182c58fe5a7c53c882298b116bf50e5f6f3d
AlexShvet <malifey39@gmail.com>
2020-05-23 21:57:15 +0700

    косметические правки

--- stop_watch.php
+++ stop_watch.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$username = $_SESSION["username"]["username"];
+$username = mb_convert_encoding($_SESSION["username"]["username"],'UTF-8');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,21 +12,21 @@ $username = $_SESSION["username"]["username"];
     <link rel="stylesheet" href="style_for_stop_watch.css">
 </head>
 <body>
-    <div>Сайт Александра приветствует Вас <?php echo $username ?>!</div>
+    <div id="greeting">Сайт Александра</div><br><div id="greeting">приветствует Вас <?php echo $username ?>!</div>
     <div id="notice">Что бы обнулить время, нажми сначала кнопку "Пуск"</div>
-        <div id="stop_watch" align="center">
+        <div id="stop_watch">
             <div id="zero" onclick="zero()">Обнулить</div>
             <div id="sta" onclick="start()">Пуск</div>
             <div id="null_min">0</div>
             <div id="minutes">0</div>
             <div id="null_sec">0</div>
-            <div id="seconds">0</div>
+            <div id="seconds">0</div><br>
             <div id="two" onclick="pause()">Пауза</div>
         </div>
         <div align="center">
             <form method="POST" action="write_result.php">
-                <input id="weight" type="numder" name="weight" placeholder="сюда вес (штанги)" required><br>
-                <input id="many_times" type="number" name="many_times" placeholder="тут разы" required><br>
+                <input id="weight" type="number" name="weight" placeholder="    сюда вес (штанги)" required><br>
+                <input id="many_times" type="number" name="many_times" placeholder="    тут разы" required><br>
                 <input type="submit" value="готово">
             </form>
         </div>
