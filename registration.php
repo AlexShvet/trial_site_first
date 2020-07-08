5fb231a6745b1d19777bc9d3a7fe720fc832321c
AlexShvet <malifey39@gmail.com>
2020-07-08 22:20:44 +0700

    добавил почту при регистрации

--- registration.php
+++ registration.php
@@ -8,6 +8,7 @@ if (mysqli_connect_errno()) {
 if(isset($_POST["submit"])){
     $username = htmlspecialchars($_POST["username"]);
     $login = htmlspecialchars($_POST["login"]);
+    $email = htmlspecialchars($_POST["email"]);
     $password = md5($_POST["password"]);
     $chek_exist = mysqli_query($link,"SELECT * FROM reg_for_stopwatch WHERE (`login` = '$login' AND `password` = '$password') LIMIT 1")->fetch_assoc();
     if (isset($chek_exist)) {
@@ -15,11 +16,11 @@ if(isset($_POST["submit"])){
         $_SESSION["login"] = $login;
         $_SESSION["message"] = "Такой пользователь уже есть";
         Header("Location: main_stop_watch.php");
-    } else reg_and_write($link,$username,$login,$password);
+    } else reg_and_write($link,$username,$login,$password,$email);
     
 }
-function reg_and_write($link,$username,$login,$password){
-    $b = mysqli_query($link,"INSERT INTO reg_for_stopwatch (`username`,`login`,`password`) VALUES ('$username','$login','$password');");
+function reg_and_write($link,$username,$login,$password,$email){
+    $b = mysqli_query($link,"INSERT INTO reg_for_stopwatch (`username`,`login`,`password`,`email`) VALUES ('$username','$login','$password','$email');");
     $create_table_for_write_result = 'write_result_'.$login;
     mysqli_query($link,"CREATE TABLE `$create_table_for_write_result` (id INT PRIMARY KEY AUTO_INCREMENT, weight VARCHAR(50), many_times VARCHAR(50), date_time DATETIME);");
     Header("Location: stop_watch.php");
@@ -42,6 +43,7 @@ mysqli_close($link);
         <input type="text" maxlenght="50" name="username" placeholder="сюда имя" required><br><br>
         <input type="text"  maxlenght="20" name="login" placeholder="тут логин" required><br><br>
         <input type="text" maxlenght="20" name="password" placeholder="здесь пароль" required><br><br>
+        <input type="email"  maxlenght="20" name="email" placeholder="это для почты" required><br><br>
         <input name="submit" type="submit" value="готово"><br><br>
         <input type="reset" value="очистить поля формы">
     </form>
